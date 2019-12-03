a9d63adbabdb55a59dd11542c4c3e90380d12f7e
Guenton <Guenton@Gmail.com>
2019-12-03 17:09:41 -0400

    relaunch

---
+++ pages/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <blockquote class="blockquote">
+        &#8220;Wie niet dom is moet slim zijn.&#8221;
+        <footer>
+          <small>
+            <em>&mdash; Marlon C. G. Richardson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
